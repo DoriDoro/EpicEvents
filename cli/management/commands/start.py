96f37a16c45c62c51e2e7de0035bc2f336297d0b
doridoro <dorothea.reher@gmail.com>
2024-01-11 16:01:53 +0100

    create cli with menu and start(ing point) for CLI

---
+++ cli/management/commands/start.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+from cli.menu import get_start_menu
+
+
+class Command(BaseCommand):
+    help = "Start of the program."
+
+    def handle(self, *args, **options):
+        choice = get_start_menu("Welcome to Epic Events")
+
+        if choice == 1:
+            call_command("account_manager")
+        if choice == 2:
+            call_command("contract")
+        if choice == 3:
+            call_command("event")
---
+++ cli/menu.py
@@ -0,0 +1,35 @@
+def get_start_menu(title):
+    print(f"** {title} **", end="\n\n")
+
+    print(" *** Main Menu *** ")
+    print("  [1] Manage the account managers")
+    print("  [2] Manage the contracts")
+    print("  [3] Manage the events")
+    print("  [4] Quit program", end="\n\n")
+    choice = int(input("Please enter your choice: "))
+    print(choice)
+    print()
+
+    return choice
+
+
+def get_app_menu(title, app):
+    print(f"** {title} **", end="\n\n")
+
+    print(f" *** Menu of the {app} *** ")
+    if app == "Main":
+        print("  [1] Manage the account managers")
+        print("  [2] Manage the contracts")
+        print("  [3] Manage the events")
+        print("  [4] Quit program", end="\n\n")
+        choice = int(input("Please enter your choice: "))
+        print()
+    if app != "Main":
+        print(f"  [1] Create an {app}")
+        print(f"  [2] Update an {app}")
+        print(f"  [3] Delete an {app}")
+        print(f"  [4] go back to Main Menu", end="\n\n")
+        choice = int(input("Please enter your choice: "))
+        print()
+
+    return choice
