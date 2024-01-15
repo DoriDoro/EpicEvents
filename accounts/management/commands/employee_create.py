f210d1486d082e0387e8a36af9af1ac0121f300e
doridoro <dorothea.reher@gmail.com>
2024-01-15 17:38:07 +0100

    improve employee_create with loops and choices

--- accounts/management/commands/employee_create.py
+++ accounts/management/commands/employee_create.py
@@ -11,48 +11,47 @@ class Command(BaseCommand):
     help = "Creates a new employee."
 
     def handle(self, *args, **options):
-        email = str(
-            input("Please enter the email address of the future employee: ")
-        )  # search for the email address of user
-        user = UserModel.objects.filter(email=email).first()
-        if user is None:
-            print(
-                "This email address is not known. Please enter a valid email address."
-            )  # improve this logic, implement while loop AND quit the program
-            while True:
-                try:
-                    choice = int(input(" Please enter your choice: "))
-                    print(choice)
-                    print()
-                    break
-                except ValueError:
-                    print("   Invalid input. Please enter a number.", end="\n\n")
-
-        f_name = str(input("Please enter the first name: "))
-        l_name = str(input("Please enter the last name: "))
-        role = str(
-            input("Please enter the role: ")
-        )  # improve to make a choice between possible roles
-        # while loop until new employee is created
-        user_id = options["user"]
-        user = UserModel.objects.filter(pk=user_id).first()
-        # search for employee with role to verify their existence
-        employee_exists = Employee.objects.filter(user=user).exists()
-
-        if employee_exists:
-            print()
-            self.stdout.write(
-                f"This employee: {user} with role: {options['role']} exists already!"
+        while True:
+            email = str(
+                input(" Please enter the email address of the future employee: ")
             )
-            print()
-            call_command("start")
+            user = UserModel.objects.filter(email=email).first()
+            if user is None:
+                print(
+                    "   This email address is not known. Please enter a valid email address."
+                )
+            else:
+                f_name = str(input(" Please enter the first name: "))
+                l_name = str(input(" Please enter the last name: "))
 
-        employee = Employee(
-            user=user,
-            first_name=options["first_name"],
-            last_name=options["last_name"],
-            role=options["role"],
-        )
-        employee.save()
+                print("  Choose the role of your employee:")
+                print(f"   [1] Sales")
+                print(f"   [2] Support")
+                print(f"   [3] Management")
+                while True:
+                    try:
+                        role = int(input(" Please enter your choice for the role: "))
+                        print(role)
+                        break
+                    except ValueError:
+                        print("   Invalid input. Please enter a number.", end="\n\n")
 
-        self.stdout.write("New employee was created.")
+                employee_exists = Employee.objects.filter(user=user).exists()
+
+                if employee_exists:
+                    print()
+                    self.stdout.write(
+                        f"   This employee: {user} with role: {role} exists already!"
+                    )
+                    print()
+                else:
+                    employee = Employee(
+                        user=user,
+                        first_name=f_name,
+                        last_name=l_name,
+                        role=role,
+                    )
+                    employee.save()
+                    print()
+                    self.stdout.write("   A new employee was created.")
+                    break
