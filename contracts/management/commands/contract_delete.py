70b31e172a6c2e609be1ea7463018b766b4a37d6
doridoro <dorothea.reher@gmail.com>
2024-02-06 16:39:12 +0100

    create contract update and delete

---
+++ contracts/management/commands/contract_delete.py
@@ -0,0 +1,66 @@
+from django.core.management import call_command
+
+from accounts.models import Employee
+from cli.utils_custom_command import EpicEventsCommand
+from cli.utils_messages import create_success_message, create_invalid_error_message
+from cli.utils_tables import (
+    create_model_table,
+    create_pretty_table,
+    create_queryset_table,
+)
+from contracts.models import Contract
+
+
+class Command(EpicEventsCommand):
+    help = "Prompts for details to a contract."
+    action = "DELETE"
+
+    def get_create_model_table(self):
+        table_data = dict()
+
+        queryset = Contract.objects.select_related("client").all()
+
+        for contract in queryset:
+            table_data[contract.client.id] = contract.client.email
+
+        create_queryset_table(table_data, "Email", "Client Emails from contracts")
+
+    def get_requested_model(self):
+        while True:
+            email = self.email_input("Email address")
+            self.object = Contract.objects.filter(client__email=email).first()
+
+            if self.object:
+                break
+            else:
+                create_invalid_error_message("email")
+
+        self.stdout.write()
+        contract_table = [
+            ["Client: ", self.object.client.email],
+            ["Employee: ", self.object.employee.user.email],
+            ["Total costs: ", self.object.total_costs],
+            ["Amount paid: ", self.object.amount_paid],
+            ["State: ", self.object.state],
+        ]
+        create_pretty_table(contract_table, "Details of the Contract: ")
+
+    def get_data(self):
+        return {
+            "delete": self.choice_str_input(
+                ("Y", "N"), "Choice to delete [Y]es or [N]o"
+            ),
+        }
+
+    def make_changes(self, data):
+        if data["delete"] == "Y":
+            self.object.delete()
+        if data["delete"] == "N":
+            self.stdout.write()
+            call_command("contract")
+
+    def display_changes(self):
+        create_success_message("Contract", "deleted")
+
+    def go_back(self):
+        call_command("contract")
---
+++ contracts/management/commands/contract_update.py
@@ -0,0 +1,130 @@
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.db import transaction
+
+from accounts.models import Employee
+from cli.utils_custom_command import EpicEventsCommand
+from cli.utils_messages import (
+    create_success_message,
+    create_invalid_error_message,
+)
+from cli.utils_tables import (
+    create_queryset_table,
+    create_pretty_table,
+)
+from contracts.models import Contract
+
+UserModel = get_user_model()
+
+
+class Command(EpicEventsCommand):
+    help = "Prompts for details to update a contract."
+    action = "UPDATE"
+
+    def get_create_model_table(self):
+        table_data = dict()
+
+        queryset = Contract.objects.select_related("client").all()
+
+        for contract in queryset:
+            table_data[contract.client.id] = contract.client.email
+
+        create_queryset_table(table_data, "Email", "Client Emails from contracts")
+
+    def get_requested_model(self):
+        while True:
+            email = self.email_input("Email address")
+            self.object = Contract.objects.filter(client__email=email).first()
+
+            if self.object:
+                break
+            else:
+                create_invalid_error_message("email")
+
+        self.stdout.write()
+        contract_table = [
+            ["Client: ", self.object.client.email],
+            ["[E]mployee: ", self.object.employee.user.email],
+            ["[T]otal costs: ", self.object.total_costs],
+            ["[A]mount paid: ", self.object.amount_paid],
+            ["[S]tate: ", self.object.state],
+        ]
+        create_pretty_table(contract_table, "Details of the Contract: ")
+
+    def get_fields_to_update(self):
+        self.fields_to_update = self.multiple_choice_str_input(
+            ("E", "T", "A", "S"), "Your choice? [E, T, A, S]"
+        )
+
+    def get_available_fields(self):
+        self.available_fields = {
+            "E": {
+                "method": self.email_input,
+                "params": {"label": "Employee Email"},
+                "label": "employee_email",
+            },
+            "T": {
+                "method": self.int_input,
+                "params": {"label": "Total amount"},
+                "label": "total_costs",
+            },
+            "A": {
+                "method": self.int_input,
+                "params": {"label": "Amount paid"},
+                "label": "amount_paid",
+            },
+            "S": {
+                "method": self.choice_str_input,
+                "params": {"options": ("S", "D"), "label": "State [S]igned or [D]raft"},
+                "label": "state",
+            },
+        }
+        return self.available_fields
+
+    def get_data(self):
+        self.update_fields = list()
+        data = dict()
+        for letter in self.fields_to_update:
+            if self.available_fields[letter]:
+                field_data = self.available_fields.get(letter)
+                method = field_data["method"]
+                params = field_data["params"]
+                label = field_data["label"]
+
+                data[label] = method(**params)
+                self.update_fields.append(label)
+
+        return data
+
+    @transaction.atomic
+    def make_changes(self, data):
+        employee_email = data.pop("employee_email", None)
+        employee = Employee.objects.filter(user__email=employee_email).first()
+
+        if employee_email:
+            contract = self.object
+            contract.employee = employee
+            contract.save()
+
+        Contract.objects.filter(client=self.object.client).update(**data)
+
+        # Refresh the object from the database
+        self.object.refresh_from_db()
+
+        return self.object
+
+    def display_changes(self):
+        self.update_fields = [
+            "total_costs",
+            "amount_paid",
+            "state",
+        ]
+        self.update_table = []
+
+        create_success_message("Contract", "updated")
+        self.update_table.append([f"Client: ", self.object.client.email])
+        self.update_table.append([f"Employee: ", self.object.employee.user.email])
+        super().display_changes()
+
+    def go_back(self):
+        call_command("contract")
