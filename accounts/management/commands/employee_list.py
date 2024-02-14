1ec604648d5bdb4c61db0c60d59173f891fe2c3e
doridoro <dorothea.reher@gmail.com>
2024-02-14 21:09:49 +0100

    modified get_create_model_table to display a detailed table of every employee.

--- accounts/management/commands/employee_list.py
+++ accounts/management/commands/employee_list.py
@@ -2,7 +2,7 @@ from django.core.management import call_command
 
 from accounts.models import Employee
 from cli.utils_custom_command import EpicEventsCommand
-from cli.utils_tables import create_model_table
+from cli.utils_tables import create_queryset_table
 
 
 class Command(EpicEventsCommand):
@@ -11,9 +11,21 @@ class Command(EpicEventsCommand):
     permissions = ["SA", "SU", "MA"]
 
     def get_create_model_table(self):
-        # TODO: more details of the employee [email, role]
-        # TODO: column_label as *arg and as parameter a list of column_labels ['email', 'role']
-        create_model_table(Employee, "user.email", "Employee Emails")
+        table_data = dict()
+
+        queryset = Employee.objects.select_related("user").all()
+        headers = ["", "email", "first name", "last name", "role"]
+
+        for employee in queryset:
+            employee_data = {
+                "email": employee.user.email,
+                "first_name": employee.first_name,
+                "last_name": employee.last_name,
+                "role": employee.role,
+            }
+            table_data[f"Employee {employee.id}"] = employee_data
+
+        create_queryset_table(table_data, "Test", headers=headers)
 
     def go_back(self):
         call_command("employee")
