e5d433c61ffef4eaad84c46112aeae40872ae992
doridoro <dorothea.reher@gmail.com>
2024-02-22 14:42:40 +0100

    corrected data_creation of contracts

--- data/management/commands/data_create_contract.py
+++ data/management/commands/data_create_contract.py
@@ -13,31 +13,28 @@ class Command(DataCreateCommand):
     help = "Creates 20 contracts."
 
     def get_queryset(self):
-        self.client = Client.objects.all().order_by("?")
-        self.employee = Employee.objects.filter(role="MA").order_by("?")
-        # TODO: The MA creates the contract but used the SA employee associated with client
+        self.client = Client.objects.select_related("employee").all().order_by("?")
 
     def create_fake_data(self):
         state_choices = ["S", "S", "S", "D"]
         data_contract = {}
 
         for i in range(1, 21):
-            client = self.client[(i - 1) % len(self.client)]
-            employee = self.employee[(i - 1) % len(self.employee)]
-            state = state_choices[(i - 1) % len(state_choices)]
+            for client in self.client:
+                state = state_choices[(i - 1) % len(state_choices)]
 
-            contract = {
-                "client": client,
-                "employee": employee,
-                "total_costs": fake.pydecimal(
-                    left_digits=5, right_digits=2, positive=True
-                ),
-                "amount_paid": fake.pydecimal(
-                    left_digits=3, right_digits=2, positive=True
-                ),
-                "state": state,
-            }
-            data_contract[i] = contract
+                contract = {
+                    "client": client,
+                    "employee": client.employee,
+                    "total_costs": fake.pydecimal(
+                        left_digits=5, right_digits=2, positive=True
+                    ),
+                    "amount_paid": fake.pydecimal(
+                        left_digits=3, right_digits=2, positive=True
+                    ),
+                    "state": state,
+                }
+                data_contract[i] = contract
 
         return data_contract
 
