247d1a78067a10c1d90968cf5cfbe6245f3d446d
doridoro <dorothea.reher@gmail.com>
2024-02-27 16:43:17 +0100

    modified custom create_user, if password is empty string or None raise ValueError and validate the password.

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.password_validation import validate_password
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
@@ -29,6 +30,11 @@ class UserManager(BaseUserManager):
         """
         if not email:
             raise ValueError(_("The Email must be set"))
+        if not password:
+            raise ValueError(_("The Password must be set"))
+
+        password = validate_password(password)
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
